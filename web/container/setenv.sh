a249653c1221d242b057c2f5041131bb70beaff8
Hange Chen <Hange@Admins-MacBook-Pro.local>
2020-05-27 23:40:47 -0400

    test10526

--- web/container/setenv.sh
+++ web/container/setenv.sh
@@ -1,6 +1,6 @@
 export CATALINA_OPTS="$CATALINA_OPTS -Ddatabase.driver=org.postgresql.Driver"
 export CATALINA_OPTS="$CATALINA_OPTS -Ddatabase.dialect=org.hibernate.dialect.PostgreSQL9Dialect"
-export CATALINA_OPTS="$CATALINA_OPTS -Ddatabase.url=jdbc:postgresql://172.17.0.2:5432/nba"
+export CATALINA_OPTS="$CATALINA_OPTS -Ddatabase.url=jdbc:postgresql://172.17.0.3:5432/nba"
 export CATALINA_OPTS="$CATALINA_OPTS -Ddatabase.user=admin"
 export CATALINA_OPTS="$CATALINA_OPTS -Ddatabase.password=password"
 export CATALINA_OPTS="$CATALINA_OPTS -Dspring.profiles.active=dev"
